45bfaa533c1200117ba12308347d226a29a64054
pzhudjs01 <pzhudjs01@2-3>
2015-04-02 16:11:42 +0800

    add a constructor to Address.java

--- src/com/shop/domain/Address.java
+++ src/com/shop/domain/Address.java
@@ -5,6 +5,14 @@ public class Address {
 	private String name;
 	private String parentid;
 	private String orderfield;
+	
+	//新增构造方法
+	public Address(String id, String name, String parentid, String orderfield) {
+		this.id = id;
+		this.name = name;
+		this.parentid = parentid;
+		this.orderfield = orderfield;
+	}
 	public String getId() {
 		return id;
 	}
